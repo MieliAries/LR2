d763e9f6ead700f112bca97d796b92db3496704b
MieliAries <81920857+MieliAries@users.noreply.github.com>
2021-04-04 22:07:06 +0300

    Update main.php

--- development/apps/courses/model/main.php
+++ development/apps/courses/model/main.php
@@ -13,9 +13,9 @@ class Main
 
 	public function formsubmitAmbassador(array $data):?array {
 		// Тут модель повинна бути допрацьована, щоб використовувати бази даних, тощо
-		$key = '1589139881:AAELl83iHhQutMHTlZ0GB4i3Rag4h9iJLj4'; // Ключ API телеграм
+		$key = ''; // Ключ API телеграм
 		$result = null;
-		$chat = 434330418;
+		$chat = ;
 		$text = "Нова заявка в *Цифрові Амбасадори*:\n" . $data['firstname'] . ' '. $data['secondname']. ', '. $data['position'] . "\n*Зв'язок*: " . $data['phone'];
 		$text = urlencode($text);
 		$answer = file_get_contents("https://api.telegram.org/bot$key/sendMessage?parse_mode=markdown&chat_id=$chat&text=$text");
